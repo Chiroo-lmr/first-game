1f8aa6051a9a46881fb3a0ea69dd31099cab62b6
Chirootao <126762943+Chirootao@users.noreply.github.com>
2024-01-28 19:49:27 +0100

    +- création u.i.
    bon enfait ct le truc de test mais jsplus ce que j'ai fais bref mise en place de l'ui à moitié ( c'est bugué )

---
+++ scripts/hud.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+func _ready():
+	$startButton.visible = true
+
+func _physics_process(delta):
+	$Score.text = "Ennemis tués : " + str(Global.enemiesKilled)
+	Global.restartButton = $RestartButton
+	Global.QuitButton = $QuitButton
+	$gameOver.visible = false
+	if Global.gameOver == true:
+		$RestartButton.visible = true
+		$gameOver.visible = true
+		$QuitButton.visible = true
+	if Global.gameStart == true:
+		$startButton.visible = false
+	
+	
+func _on_start_button_pressed():
+	Global.buttonPressed = true
+	$startButton.visible = false
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+func _on_escape_restart_button_pressed():
+	Global.gameStart = true
+	Global.gamePause = false
+	Global.gameOver = false
+	Global.killed_enemies = []
+	Global.howManyEnemies = []
+	Global.enemiesKilled = 0
+	Global.playerHealth = 100
+	get_tree().reload_current_scene()
+	
