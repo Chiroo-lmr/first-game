f6abf31e39ce7d0e6fcb050e4622940c5d3d5374
Chirootao <126762943+Chirootao@users.noreply.github.com>
2024-02-03 15:07:54 +0100

    FixingChangeScenes

--- scripts/cliff_side_2.gd
+++ scripts/cliff_side_2.gd
@@ -3,7 +3,7 @@ extends Node2D
 func _on_collision_transition_scene_body_entered(body):
 	if body.has_method("player"):
 		Global.transitionScene = true
-		get_tree().change_scene_to_file("res://scenes/world.tscn")
+		get_tree().change_scene_to_file("res://scenes/World.tscn")
 		Global.currentScene = "world"
 		Global.playerCurrentPosition = Vector2(471, 265)
 		
--- scripts/world.gd
+++ scripts/world.gd
@@ -65,6 +65,3 @@ func spawnEnemies():
 
 
 
-
-
-
