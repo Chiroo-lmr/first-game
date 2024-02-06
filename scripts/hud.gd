ca011df1ef1fedd3ceb4f6dc34c4fbd6506707df
Chirootao <126762943+Chirootao@users.noreply.github.com>
2024-02-06 23:00:18 +0100

    pause/depause with ui_cancel works

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -51,20 +51,32 @@ func _physics_process(delta):
 		restartButton.visible = true
 		labelGameOver.visible = true
 		QuitButton.visible = true
+		print(Global.gameStart)
 	if Global.gameStart == true:
 		Global.gameOver = false
 		Global.gamePause = false
 		Global.gameLaunch == false
 		startButton.visible = false
+		print(Global.gameStart)
 	if Global.gameLaunch == true:
 		startButton.visible = true
-	if Input.is_action_pressed("ui_cancel") and Global.gameStart == true:
+		print(Global.gameStart)
+	if Input.is_action_just_pressed("ui_cancel"):
+		game_pause()
+		print(Global.gameStart)
+	if Global.label == 0:
+		labelScore.visible = false
+
+func game_pause():
+	if Global.gameStart == true:
 		Global.gameStart = false
 		Global.gamePause = true
 		QuitButton.visible = true
 		restartButton.visible = true
 		depauseButton.visible = true
-	if Global.label == 0:
-		labelScore.visible = false
-
-
+	else:
+		Global.gameStart = true
+		Global.gamePause = false
+		QuitButton.visible = false
+		restartButton.visible = false
+		depauseButton.visible = false
